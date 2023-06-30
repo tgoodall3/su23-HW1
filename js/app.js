afec0c1baf499df6934593cfa2c23fb682ed4081
tgoodall3 <Tylergoodall3@gmail.com>
2023-06-30 17:23:21 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+function setup(){
+    createCanvas(800,600);
+    arc(444, 340, 350, 350, 150, PI / 2.0);
+    fill(165,12,14);
+    ellipse(450,250,280,280);
+    fill(190,12,14)
+    ellipse(450,250,180,180);
+    fill(0)
+    ellipse(450,250,60,60);
+    ellipse(360,250,30,30)
+    ellipse(489,170,30,30)
+    ellipse(455,340,30,30)
+    arc(349, 250, 50, 50, 50, PI / 2.0);
+    arc(478, 170, 50, 50, 50, PI / 2.0);
+    arc(444, 340, 50, 50, 50, PI / 2.0);
+
+
+}
+
+// function draw(){
+//     background(220);
+//     fill("A50C0E");
+//     ellipse(450,250,280,280);
+//     ellipse(450,250,180,180);
+//     ellipse(450,250,60,60);
+//     }
+
+
+// }
+
+// function draw() {
+//     if (mouseIsPressed) {
+//       fill('A50C0E');
+//     } else {
+//       fill('white');
+//     }
+//     background(220);
+//     ellipse(450,250,280,280);
+//     ellipse(450,250,180,180);
+//     ellipse(450,250,60,60);;
+//   }
